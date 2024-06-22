08eb2a89d370efa93b1fee52c1b4e8dd2fe686a2
Šimon Macek <simon.macek@proficio.cz>
2024-06-22 20:05:30 +0200

    drawer header test icon

--- src/components/__tests__/DrawerHeader.cy.js
+++ src/components/__tests__/DrawerHeader.cy.js
@@ -1,12 +1,5 @@
-import { colors } from 'quasar';
-
 import DrawerHeader from '../global/DrawerHeader.vue';
 import { i18n } from '../../boot/i18n';
-
-const { getPaletteColor } = colors;
-const black = getPaletteColor('black');
-const white = getPaletteColor('white');
-const grey10 = getPaletteColor('grey-10');
 
 describe('<DrawerHeader>', () => {
   beforeEach(() => {
@@ -55,39 +48,14 @@ describe('<DrawerHeader>', () => {
   });
 
   it('renders help button', () => {
-    cy.dataCy('button-help')
-      .should('be.visible')
-      .and('have.css', 'font-size', '8px')
-      .and('have.css', 'font-weight', '500')
-      .and('have.backgroundColor', grey10)
-      .and('have.css', 'border-radius', '50%'); // round
-    cy.dataCy('button-help').should('contain', 'question_mark');
-  });
-
-  it('renders help button with correct size', () => {
-    cy.dataCy('button-help').should('be.visible');
-    cy.dataCy('button-help').invoke('height').should('be.equal', 24);
-    cy.dataCy('button-help').invoke('width').should('be.equal', 24);
-  });
-
-  it('renders help icon', () => {
-    cy.dataCy('icon-help')
-      .should('contain', 'question_mark')
-      .and('have.color', white);
-    cy.dataCy('icon-help')
-      .invoke('height')
-      .should('be.gt', 12)
-      .and('be.lt', 14);
-    cy.dataCy('icon-help').invoke('width').should('be.gt', 12).and('be.lt', 14);
+    cy.dataCy('help-button').should('be.visible');
   });
 
   it('renders notifications icon', () => {
     cy.window().then(() => {
-      cy.dataCy('icon-notification')
-        .should('be.visible')
-        .and('have.color', black)
-        .and('have.css', 'width', '24px')
-        .and('contain.text', 'notifications');
+      cy.dataCy('drawer-header-notification-icon').then((element) => {
+        cy.testIcon({ element, name: 'drawer-header-notification', size: 24 });
+      });
     });
   });
 
