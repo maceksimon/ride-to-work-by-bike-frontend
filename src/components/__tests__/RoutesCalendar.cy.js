f51fd3fecdf8752d3c47337ef24d250cb7848fae
Šimon Macek <simon.macek@proficio.cz>
2024-06-26 23:50:10 +0200

    routes calendar add calendar view with day slots

---
+++ src/components/__tests__/RoutesCalendar.cy.js
@@ -0,0 +1,36 @@
+import RoutesCalendar from 'components/routes/RoutesCalendar.vue';
+import { i18n } from '../../boot/i18n';
+
+describe('<RoutesCalendar>', () => {
+  it('has translation for all strings', () => {
+    cy.testLanguageStringsInContext([], 'index.component', i18n);
+  });
+
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.mount(RoutesCalendar, {
+        props: {},
+      });
+      cy.viewport('macbook-16');
+    });
+
+    coreTests();
+  });
+
+  context('mobile', () => {
+    beforeEach(() => {
+      cy.mount(RoutesCalendar, {
+        props: {},
+      });
+      cy.viewport('iphone-6');
+    });
+
+    coreTests();
+  });
+});
+
+function coreTests() {
+  it('renders component', () => {
+    cy.dataCy('routes-calendar').should('be.visible');
+  });
+}
