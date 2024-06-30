829502bc9a221e634ba828a50883badc41921b8a
Šimon Macek <simon.macek@proficio.cz>
2024-06-30 17:23:41 +0200

    route item display: styling + layout

--- src/components/__tests__/RouteItemDisplay.cy.js
+++ src/components/__tests__/RouteItemDisplay.cy.js
@@ -4,8 +4,8 @@ import RouteItemDisplay from 'components/routes/RouteItemDisplay.vue';
 import { i18n } from '../../boot/i18n';
 
 const { getPaletteColor } = colors;
-const black = getPaletteColor('black');
 const grey10 = getPaletteColor('grey-10');
+const primary = getPaletteColor('primary');
 const secondary = getPaletteColor('secondary');
 
 describe('<RouteItemDisplay>', () => {
@@ -36,8 +36,6 @@ describe('<RouteItemDisplay>', () => {
 
     coreTests();
 
-    desktopTests();
-
     toWorkTests();
 
     distanceTests();
@@ -56,8 +54,6 @@ describe('<RouteItemDisplay>', () => {
     });
 
     coreTests();
-
-    desktopTests();
 
     fromWorkTests();
 
@@ -80,11 +76,6 @@ describe('<RouteItemDisplay>', () => {
     });
 
     coreTests();
-
-    it('renders columns stacked', () => {
-      cy.testElementPercentageWidth(cy.dataCy('column-direction'), 100);
-      cy.testElementPercentageWidth(cy.dataCy('column-distance'), 100);
-    });
   });
 });
 
@@ -119,31 +110,21 @@ function coreTests() {
     // icon transport
     cy.dataCy('icon-transport')
       .should('be.visible')
-      .invoke('height')
-      .should('be.eq', 18);
-    cy.dataCy('icon-transport')
-      .should('be.visible')
-      .invoke('width')
-      .should('be.eq', 18);
+      .and('have.css', 'font-size', '18px')
+      .and('have.color', primary);
+    cy.dataCy('icon-transport').invoke('height').should('be.eq', 18);
+    cy.dataCy('icon-transport').invoke('width').should('be.eq', 18);
     // description transport styles
     cy.dataCy('description-transport')
       .should('be.visible')
       .and('have.css', 'font-size', '14px')
       .and('have.css', 'font-weight', '400')
-      .and('have.color', black);
+      .and('have.color', grey10);
   });
-}
 
-function desktopTests() {
-  it('renders columns side by side in 2:10 ratio', () => {
-    cy.testElementPercentageWidth(
-      cy.dataCy('column-direction'),
-      (2 / 12) * 100,
-    );
-    cy.testElementPercentageWidth(
-      cy.dataCy('column-distance'),
-      (10 / 12) * 100,
-    );
+  it('renders two sections stacked', () => {
+    cy.testElementPercentageWidth(cy.dataCy('section-direction'), 100);
+    cy.testElementPercentageWidth(cy.dataCy('section-distance'), 100);
   });
 }
 
@@ -180,8 +161,8 @@ function distanceTests() {
       cy.dataCy('label-distance')
         .should('be.visible')
         .and('have.css', 'font-size', '14px')
-        .and('have.css', 'font-weight', '400')
-        .and('have.color', black)
+        .and('have.css', 'font-weight', '700')
+        .and('have.color', grey10)
         .and('contain', routes.toWork.distance)
         .and('contain', i18n.global.t('global.routeLengthUnit'));
     });
