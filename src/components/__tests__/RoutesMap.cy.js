60cdf568e834921caa114ccf03c64f6e5c8a5dfd
Šimon Macek <simon.macek@proficio.cz>
2024-07-02 23:36:09 +0200

    integrate openlayers library

---
+++ src/components/__tests__/RoutesMap.cy.js
@@ -0,0 +1,36 @@
+import RoutesMap from 'components/routes/RoutesMap.vue';
+import { i18n } from '../../boot/i18n';
+
+describe('<RoutesMap>', () => {
+  it('has translation for all strings', () => {
+    cy.testLanguageStringsInContext([], 'index.component', i18n);
+  });
+
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.mount(RoutesMap, {
+        props: {},
+      });
+      cy.viewport('macbook-16');
+    });
+
+    coreTests();
+  });
+
+  context('mobile', () => {
+    beforeEach(() => {
+      cy.mount(RoutesMap, {
+        props: {},
+      });
+      cy.viewport('iphone-6');
+    });
+
+    coreTests();
+  });
+});
+
+function coreTests() {
+  it('renders component', () => {
+    cy.dataCy('routes-map').should('be.visible');
+  });
+}
