5e36540c365a282f177e7a5efbec59d33849873b
Šimon Macek <simon.macek@proficio.cz>
2024-07-04 07:54:51 +0200

    routes map docs

--- src/boot/openlayers.js
+++ src/boot/openlayers.js
@@ -2,12 +2,14 @@ import { boot } from 'quasar/wrappers';
 
 import OpenLayersMap from 'vue3-openlayers';
 
-// const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
-// const options = {
-//   debug: !isProduction,
-// };
+const options = {
+  debug: !isProduction,
+};
 
 export default boot(({ app }) => {
-  app.use(OpenLayersMap);
+  app.use(OpenLayersMap, {
+    options,
+  });
 });
