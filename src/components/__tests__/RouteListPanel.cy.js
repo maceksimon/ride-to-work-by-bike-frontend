dc1800a6953291435c4afbdc05e63eb89c7a5bf1
Šimon Macek <simon.macek@proficio.cz>
2024-07-04 08:51:16 +0200

    boilerplate for bottom panel

---
+++ src/components/__tests__/RouteListPanel.cy.js
@@ -0,0 +1,36 @@
+import RouteListPanel from 'components/routes/RouteListPanel.vue';
+import { i18n } from '../../boot/i18n';
+
+describe('<RouteListPanel>', () => {
+  it('has translation for all strings', () => {
+    cy.testLanguageStringsInContext([], 'index.component', i18n);
+  });
+
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.mount(RouteListPanel, {
+        props: {},
+      });
+      cy.viewport('macbook-16');
+    });
+
+    coreTests();
+  });
+
+  context('mobile', () => {
+    beforeEach(() => {
+      cy.mount(RouteListPanel, {
+        props: {},
+      });
+      cy.viewport('iphone-6');
+    });
+
+    coreTests();
+  });
+});
+
+function coreTests() {
+  it('renders component', () => {
+    cy.dataCy('component').should('be.visible');
+  });
+}
