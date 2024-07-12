b8fc2bb257938e226471374fcc78b6e56e84f45b
Šimon Macek <simon.macek@proficio.cz>
2024-07-12 09:26:22 +0200

    options inject

--- src/boot/openlayers.js
+++ src/boot/openlayers.js
@@ -2,14 +2,6 @@ import { boot } from 'quasar/wrappers';
 
 import OpenLayersMap from 'vue3-openlayers';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const options = {
-  debug: !isProduction,
-};
-
 export default boot(({ app }) => {
-  app.use(OpenLayersMap, {
-    options,
-  });
+  app.use(OpenLayersMap);
 });
