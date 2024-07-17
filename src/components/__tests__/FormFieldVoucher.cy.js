e7c0199f2bc5df8b6fa8c1cb3613caab5956bfdb
Šimon Macek <simon.macek@proficio.cz>
2024-07-17 15:21:14 +0200

    refactor test selectors and variables

--- src/components/__tests__/FormFieldVoucher.cy.js
+++ src/components/__tests__/FormFieldVoucher.cy.js
@@ -1,6 +1,16 @@
 import FormFieldVoucher from 'components/form/FormFieldVoucher.vue';
 import { i18n } from '../../boot/i18n';
 
+// selectors
+const selectorFormFieldVoucher = 'form-field-voucher';
+const selecotrFormFieldVoucherInput = 'form-field-voucher-input';
+const selectorFormFieldVoucherSubmit = 'form-field-voucher-submit';
+const selectorQNotifyMessage = '.q-notification__message';
+
+const codeFull = 'FULL';
+const codeHalf = 'HALF';
+const codeInvalid = 'ABCD';
+
 describe('<FormFieldVoucher>', () => {
   it('has translation for all strings', () => {
     cy.testLanguageStringsInContext(
@@ -35,21 +45,46 @@ describe('<FormFieldVoucher>', () => {
 
 function coreTests() {
   it('renders component', () => {
-    cy.dataCy('form-field-voucher').should('be.visible');
-    cy.dataCy('form-field-voucher-input').should('be.visible');
-    cy.dataCy('form-field-voucher-submit')
+    cy.dataCy(selectorFormFieldVoucher).should('be.visible');
+    cy.dataCy(selecotrFormFieldVoucherInput).should('be.visible');
+    cy.dataCy(selectorFormFieldVoucherSubmit)
       .should('be.visible')
       .and('contain', i18n.global.t('form.buttonVoucherSubmit'));
   });
 
   it('clears input after successful submit', () => {
     // submit voucher
-    cy.dataCy('form-field-voucher-input').type('FULL');
-    cy.dataCy('form-field-voucher-submit').click();
-    cy.dataCy('form-field-voucher-input').should('have.value', '');
+    cy.dataCy(selecotrFormFieldVoucherInput).type(codeFull);
+    cy.dataCy(selectorFormFieldVoucherSubmit).click();
+    cy.dataCy(selecotrFormFieldVoucherInput)
+      .find('input')
+      .should('have.value', codeFull);
+    // user message
+    cy.get(selectorQNotifyMessage)
+      .should('be.visible')
+      .and('contain', i18n.global.t('notify.voucherApplySuccess'));
+    cy.dataCy(selecotrFormFieldVoucherInput).clear();
     // submit voucher
-    cy.dataCy('form-field-voucher-input').type('HALF');
-    cy.dataCy('form-field-voucher-submit').click();
-    cy.dataCy('form-field-voucher-input').should('have.value', '');
+    cy.dataCy(selecotrFormFieldVoucherInput).type(codeHalf);
+    cy.dataCy(selectorFormFieldVoucherSubmit).click();
+    cy.dataCy(selecotrFormFieldVoucherInput)
+      .find('input')
+      .should('have.value', codeHalf);
+    // user message
+    cy.get(selectorQNotifyMessage)
+      .should('be.visible')
+      .and('contain', i18n.global.t('notify.voucherApplySuccess'));
+    cy.dataCy(selecotrFormFieldVoucherInput).clear();
+    // submit invalid voucher
+    cy.dataCy(selecotrFormFieldVoucherInput).type(codeInvalid);
+    cy.dataCy(selectorFormFieldVoucherSubmit).click();
+    cy.dataCy(selecotrFormFieldVoucherInput)
+      .find('input')
+      .should('have.value', codeInvalid);
+    // user message
+    cy.get(selectorQNotifyMessage)
+      .should('be.visible')
+      .and('contain', i18n.global.t('notify.voucherApplyError'));
+    cy.dataCy(selecotrFormFieldVoucherInput).clear();
   });
 }
