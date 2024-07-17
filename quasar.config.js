e2a3072e943e05797b3341409e4ca6feff475ee5
Šimon Macek <simon.macek@proficio.cz>
2024-07-17 13:04:52 +0200

    replace console log with quasar notify

--- quasar.config.js
+++ quasar.config.js
@@ -109,7 +109,11 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
-      config: {},
+      config: {
+        notify: {
+          position: 'bottom-right',
+        },
+      },
 
       iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -122,7 +126,7 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: ['Notify'],
     },
 
     // animations: 'all', // --- includes all animations
