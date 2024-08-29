fc650f3b88b91583f8ead42aa7358ef8af8f9010
Šimon Macek <simon.macek@proficio.cz>
2024-08-29 21:12:28 +0200

    make gender field required

--- src/components/form/FormPersonalDetails.vue
+++ src/components/form/FormPersonalDetails.vue
@@ -27,7 +27,11 @@
 import { defineComponent, nextTick } from 'vue';
 
 // components
-import FormFieldTextRequired from 'components/global/FormFieldTextRequired.vue';
+import FormFieldTextRequired from '../global/FormFieldTextRequired.vue';
+import FormFieldRadioRequired from './FormFieldRadioRequired.vue';
+
+// composables
+import { i18n } from 'src/boot/i18n';
 
 // types
 import {
@@ -39,6 +43,7 @@ export default defineComponent({
   name: 'FormPersonalDetails',
   components: {
     FormFieldTextRequired,
+    FormFieldRadioRequired,
   },
   props: {
     formValues: {
@@ -71,11 +76,11 @@ export default defineComponent({
 
     const genderOptions: FormOption[] = [
       {
-        label: 'global.man',
+        label: i18n.global.t('global.man'),
         value: 'male',
       },
       {
-        label: 'global.woman',
+        label: i18n.global.t('global.woman'),
         value: 'female',
       },
     ];
@@ -152,23 +157,13 @@ export default defineComponent({
           {{ $t('form.personalDetails.titleGender') }}
         </label>
         <!-- Radio group -->
-        <q-option-group
-          dense
+        <form-field-radio-required
           inline
-          id="form-gender"
           v-model="personalDetails.gender"
           :options="genderOptions"
-          :rules="[(val: string) => !!val || $t('form.messageOptionRequired')]"
-          color="primary"
-          type="radio"
-          class="q-gutter-md q-mt-xs"
-          data-cy="form-gender-input"
-        >
-          <!-- Default slot: label (used so that label is translated dynamically) -->
-          <template v-slot:label="option">
-            {{ $t(option.label) }}
-          </template>
-        </q-option-group>
+          @update:model-value="onUpdate"
+          class="q-mt-sm"
+        />
       </div>
       <!-- Input: Newsletter -->
       <div class="col-12" data-cy="form-personal-details-newsletter">
