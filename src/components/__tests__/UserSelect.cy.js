5b76857a68bc998fb4250c741fec59b3549613c5
Šimon Macek <simon.macek@proficio.cz>
2024-10-10 13:48:32 +0200

    update logged user data in UserSelect

--- src/components/__tests__/UserSelect.cy.js
+++ src/components/__tests__/UserSelect.cy.js
@@ -1,6 +1,7 @@
+import { createPinia, setActivePinia } from 'pinia';
 import UserSelect from '../global/UserSelect.vue';
-import { user } from '../../mocks/layout';
 import { i18n } from '../../boot/i18n';
+import { useLoginStore } from '../../stores/login';
 
 // selectors
 const selectorUserSelectInput = 'user-select-input';
@@ -20,6 +21,7 @@ describe('<UserSelect>', () => {
 
   context('desktop', () => {
     beforeEach(() => {
+      setActivePinia(createPinia());
       cy.mount(UserSelect, {
         props: {},
       });
@@ -29,17 +31,28 @@ describe('<UserSelect>', () => {
     coreTests();
 
     it('shows user email', () => {
-      cy.dataCy(selectorUserSelectInput).should('contain', user.email);
+      cy.fixture('loggedUser').then((user) => {
+        const loginStore = useLoginStore();
+        loginStore.setUser(user);
+        cy.dataCy(selectorUserSelectInput).should('contain', user.email);
+      });
     });
 
     it('renders avatar with correct size', () => {
       cy.dataCy(selectorAvatar).invoke('height').should('equal', avatarSizeLg);
       cy.dataCy(selectorAvatar).invoke('width').should('equal', avatarSizeLg);
+      cy.dataCy(selectorAvatarImage)
+        .invoke('width')
+        .should('equal', avatarSizeLg);
+      cy.dataCy(selectorAvatarImage)
+        .invoke('height')
+        .should('equal', avatarSizeLg);
     });
   });
 
   context('mobile', () => {
     beforeEach(() => {
+      setActivePinia(createPinia());
       cy.mount(UserSelect, {
         props: {
           variant: 'mobile',
@@ -53,6 +66,12 @@ describe('<UserSelect>', () => {
     it('renders avatar with correct size', () => {
       cy.dataCy(selectorAvatar).invoke('width').should('equal', avatarSizeSm);
       cy.dataCy(selectorAvatar).invoke('height').should('equal', avatarSizeSm);
+      cy.dataCy(selectorAvatarImage)
+        .invoke('width')
+        .should('equal', avatarSizeSm);
+      cy.dataCy(selectorAvatarImage)
+        .invoke('height')
+        .should('equal', avatarSizeSm);
     });
   });
 
@@ -62,17 +81,26 @@ describe('<UserSelect>', () => {
     });
 
     it('renders rounded avatar with alt text', () => {
-      // avatar
-      cy.dataCy(selectorAvatar)
-        .should('be.visible')
-        .and('have.css', 'border-radius', '50%');
-      // image
-      cy.dataCy(selectorAvatarImage)
-        .should('be.visible')
-        .and('have.attr', 'aria-label', user.image.alt);
-      cy.dataCy(selectorAvatarImage)
-        .find('img')
-        .should('have.attr', 'src', user.image.src);
+      cy.fixture('loggedUser').then((user) => {
+        const loginStore = useLoginStore();
+        loginStore.setUser(user);
+        // avatar
+        cy.dataCy(selectorAvatar)
+          .should('be.visible')
+          .and('have.css', 'border-radius', '50%');
+        // image
+        cy.dataCy(selectorAvatarImage)
+          .should('be.visible')
+          .and(
+            'have.attr',
+            'aria-label',
+            `${user.first_name} ${user.last_name}`,
+          );
+        cy.dataCy(selectorAvatarImage)
+          .find('img')
+          .invoke('attr', 'src')
+          .should('contain', 'profile-placeholder');
+      });
     });
 
     it('shows dropdown on click', () => {
