cfbd8dd638f44e3896fe404ed31f60cfc8bbff95
Šimon Macek <simon.macek@proficio.cz>
2024-10-16 17:57:18 +0200

    revert test changes in router

--- src/router/index.ts
+++ src/router/index.ts
@@ -39,13 +39,8 @@ export default route(function (/* { store, ssrContext } */) {
     history: createHistory(process.env.VUE_ROUTER_BASE),
   });
 
-  const disable = true;
-
   // turn off auth check if in Cypress tests (except for register tests)
-  if (
-    (!window.Cypress || window.Cypress.spec.name === 'register.spec.cy.js') &&
-    !disable
-  ) {
+  if (!window.Cypress || window.Cypress.spec.name === 'register.spec.cy.js') {
     Router.beforeEach(async (to, from, next) => {
       const logger = inject('vuejs3-logger') as Logger | null;
       const loginStore = useLoginStore();
