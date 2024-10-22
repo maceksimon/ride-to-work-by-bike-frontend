3ee756a7750397cdcd83c8eb7b96fa571e2f7b04
Šimon Macek <simon.macek@proficio.cz>
2024-10-22 15:42:22 +0200

    cleanup

--- test/cypress/e2e/register.spec.cy.js
+++ test/cypress/e2e/register.spec.cy.js
@@ -147,88 +147,90 @@ describe('Register page', () => {
           );
           const apiRegisterUrl = `${apiBaseUrl}${urlApiRegister}`;
           const apiEmailVerificationUrl = `${apiBaseUrl}${urlApiHasUserVerifiedEmail}`;
-          cy.fixture('registerResponse.json').then((registerResponse) => {
-            // intercept register request
-            cy.intercept('POST', apiRegisterUrl, {
-              statusCode: httpSuccessfullStatus,
-              body: registerResponse,
-            }).as('registerRequest');
-            // intercept email verification request
-            cy.intercept('GET', apiEmailVerificationUrl, {
-              statusCode: httpSuccessfullStatus,
-              body: { has_user_verified_email_address: false },
-            }).as('emailVerificationRequest');
+          cy.fixture('loginRegisterResponseChallengeInactive.json').then(
+            (registerResponse) => {
+              // intercept register request
+              cy.intercept('POST', apiRegisterUrl, {
+                statusCode: httpSuccessfullStatus,
+                body: registerResponse,
+              }).as('registerRequest');
+              // intercept email verification request
+              cy.intercept('GET', apiEmailVerificationUrl, {
+                statusCode: httpSuccessfullStatus,
+                body: { has_user_verified_email_address: false },
+              }).as('emailVerificationRequest');
 
-            cy.clock().then((clock) => {
-              clock.setSystemTime(systemTimeLoggedIn);
-              // fill form
-              cy.dataCy(selectorFormRegisterEmail)
-                .find('input')
-                .type(testEmail);
-              cy.dataCy(selectorFormRegisterPasswordInput).type(testPassword);
-              cy.dataCy(selectorFormRegisterPasswordConfirmInput).type(
-                testPassword,
-              );
-              // accept privacy policy
-              cy.dataCy(selectorFormRegisterPrivacyConsent)
-                .should('be.visible')
-                .click('topLeft');
-              cy.dataCy(selectorFormRegisterSubmit).click();
-              // wait for request to complete
-              cy.wait('@registerRequest').then((interception) => {
-                expect(interception.request.body).to.deep.equal(
-                  registerRequestBody,
+              cy.clock().then((clock) => {
+                clock.setSystemTime(systemTimeLoggedIn);
+                // fill form
+                cy.dataCy(selectorFormRegisterEmail)
+                  .find('input')
+                  .type(testEmail);
+                cy.dataCy(selectorFormRegisterPasswordInput).type(testPassword);
+                cy.dataCy(selectorFormRegisterPasswordConfirmInput).type(
+                  testPassword,
                 );
-                expect(interception.response.body).to.deep.equal(
-                  registerResponse,
-                );
-              });
-              // redirect to verify email page
-              cy.url().should('contain', routesConf['verify_email']['path']);
-              cy.wait('@emailVerificationRequest').then((interception) => {
-                expect(interception.response.body).to.deep.equal({
-                  has_user_verified_email_address: false,
+                // accept privacy policy
+                cy.dataCy(selectorFormRegisterPrivacyConsent)
+                  .should('be.visible')
+                  .click('topLeft');
+                cy.dataCy(selectorFormRegisterSubmit).click();
+                // wait for request to complete
+                cy.wait('@registerRequest').then((interception) => {
+                  expect(interception.request.body).to.deep.equal(
+                    registerRequestBody,
+                  );
+                  expect(interception.response.body).to.deep.equal(
+                    registerResponse,
+                  );
                 });
-              });
+                // redirect to verify email page
+                cy.url().should('contain', routesConf['verify_email']['path']);
+                cy.wait('@emailVerificationRequest').then((interception) => {
+                  expect(interception.response.body).to.deep.equal({
+                    has_user_verified_email_address: false,
+                  });
+                });
 
-              // test navigating to app pages (logged in and email not verified)
-              cy.visit(
-                '#' + routesConf['routes_calendar']['children']['fullPath'],
-              );
-              cy.url().should(
-                'not.contain',
-                routesConf['routes_calendar']['children']['fullPath'],
-              );
-              cy.url().should('contain', routesConf['verify_email']['path']);
-              // results page
-              cy.visit('#' + routesConf['results']['path']);
-              cy.url().should('not.contain', routesConf['results']['path']);
-              cy.url().should('contain', routesConf['verify_email']['path']);
-              // community page
-              cy.visit('#' + routesConf['community']['path']);
-              cy.url().should('not.contain', routesConf['community']['path']);
-              cy.url().should('contain', routesConf['verify_email']['path']);
-              // prizes page
-              cy.visit('#' + routesConf['prizes']['path']);
-              cy.url().should('not.contain', routesConf['prizes']['path']);
-              cy.url().should('contain', routesConf['verify_email']['path']);
-              // profile page
-              cy.visit('#' + routesConf['profile_details']['path']);
-              cy.url().should(
-                'not.contain',
-                routesConf['profile_details']['path'],
-              );
-              cy.url().should('contain', routesConf['verify_email']['path']);
-              // test navigating to login and register page (this is NOT allowed when email not verified and logged in)
-              cy.visit('#' + routesConf['login']['path']);
-              cy.url().should('contain', routesConf['verify_email']['path']);
-              cy.visit('#' + routesConf['register']['path']);
-              cy.url().should('contain', routesConf['verify_email']['path']);
-              // test navigating to verify email page (this is allowed when email not verified)
-              cy.visit('#' + routesConf['verify_email']['path']);
-              cy.url().should('contain', routesConf['verify_email']['path']);
-            });
-          });
+                // test navigating to app pages (logged in and email not verified)
+                cy.visit(
+                  '#' + routesConf['routes_calendar']['children']['fullPath'],
+                );
+                cy.url().should(
+                  'not.contain',
+                  routesConf['routes_calendar']['children']['fullPath'],
+                );
+                cy.url().should('contain', routesConf['verify_email']['path']);
+                // results page
+                cy.visit('#' + routesConf['results']['path']);
+                cy.url().should('not.contain', routesConf['results']['path']);
+                cy.url().should('contain', routesConf['verify_email']['path']);
+                // community page
+                cy.visit('#' + routesConf['community']['path']);
+                cy.url().should('not.contain', routesConf['community']['path']);
+                cy.url().should('contain', routesConf['verify_email']['path']);
+                // prizes page
+                cy.visit('#' + routesConf['prizes']['path']);
+                cy.url().should('not.contain', routesConf['prizes']['path']);
+                cy.url().should('contain', routesConf['verify_email']['path']);
+                // profile page
+                cy.visit('#' + routesConf['profile_details']['path']);
+                cy.url().should(
+                  'not.contain',
+                  routesConf['profile_details']['path'],
+                );
+                cy.url().should('contain', routesConf['verify_email']['path']);
+                // test navigating to login and register page (this is NOT allowed when email not verified and logged in)
+                cy.visit('#' + routesConf['login']['path']);
+                cy.url().should('contain', routesConf['verify_email']['path']);
+                cy.visit('#' + routesConf['register']['path']);
+                cy.url().should('contain', routesConf['verify_email']['path']);
+                // test navigating to verify email page (this is allowed when email not verified)
+                cy.visit('#' + routesConf['verify_email']['path']);
+                cy.url().should('contain', routesConf['verify_email']['path']);
+              });
+            },
+          );
         });
       });
     });
