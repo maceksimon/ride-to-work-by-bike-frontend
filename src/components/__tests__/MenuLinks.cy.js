bfe10ea07d8f6491d7218506f4a7eddc18e9a4e3
Šimon Macek <simon.macek@proficio.cz>
2024-11-04 21:58:00 +0100

    translate titles and display them for menu links

--- src/components/__tests__/MenuLinks.cy.js
+++ src/components/__tests__/MenuLinks.cy.js
@@ -1,6 +1,5 @@
 import { colors } from 'quasar';
 import MenuLinks from '../global/MenuLinks.vue';
-import { i18n } from '../../boot/i18n';
 import { useSocialLinks } from '../../composables/useSocialLinks';
 
 // colors
@@ -45,10 +44,7 @@ describe('<MenuLinks>', () => {
       cy.dataCy('button-menu-links')
         .should('have.length', socialLinks.length)
         .each(($el, index) => {
-          cy.wrap($el).should(
-            'contain',
-            i18n.global.t(socialLinks[index].title),
-          );
+          cy.wrap($el).should('have.attr', 'title', socialLinks[index].title);
           cy.wrap($el).should('have.attr', 'href', socialLinks[index].url);
         });
       cy.dataCy('button-menu-links')
