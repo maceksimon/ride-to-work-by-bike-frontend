9e84c60c1ea990db44124a3eb36a19be2b36074b
Šimon Macek <simon.macek@proficio.cz>
2024-11-29 14:54:47 +0100

    refactor register challenge team step into a separate component

---
+++ src/components/__tests__/FormSelectTeam.cy.js
@@ -0,0 +1,48 @@
+import FormSelectTeam from 'components/form/FormSelectTeam.vue';
+import { i18n } from '../../boot/i18n';
+
+// selectors
+const selectorFormSelectTeam = 'form-select-team';
+const selectorFormSelectTableTeam = 'form-select-table-team';
+const selectorFormSelectTeamInfo = 'form-select-team-info';
+describe('<FormSelectTeam>', () => {
+  it('has translation for all strings', () => {
+    cy.testLanguageStringsInContext(
+      ['textTeamInfo'],
+      'register.challenge',
+      i18n,
+    );
+  });
+
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.mount(FormSelectTeam, {
+        props: {},
+      });
+      cy.viewport('macbook-16');
+    });
+
+    coreTests();
+  });
+
+  context('mobile', () => {
+    beforeEach(() => {
+      cy.mount(FormSelectTeam, {
+        props: {},
+      });
+      cy.viewport('iphone-6');
+    });
+
+    coreTests();
+  });
+});
+
+function coreTests() {
+  it('renders component', () => {
+    cy.dataCy(selectorFormSelectTeam).should('be.visible');
+    cy.dataCy(selectorFormSelectTableTeam).should('be.visible');
+    cy.dataCy(selectorFormSelectTeamInfo)
+      .should('be.visible')
+      .should('have.text', i18n.global.t('register.challenge.textTeamInfo'));
+  });
+}
