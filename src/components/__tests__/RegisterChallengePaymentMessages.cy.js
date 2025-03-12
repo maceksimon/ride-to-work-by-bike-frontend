4a0a15e6abcdb53a4082f1e7cfbbb097bb183bdb
Tomas Zigo <tomas.zigo@slovanet.sk>
2025-03-12 05:37:24 +0100

    src/components/__tests__: fix RegisterChallengePaymentMessages component test

--- src/components/__tests__/RegisterChallengePaymentMessages.cy.js
+++ src/components/__tests__/RegisterChallengePaymentMessages.cy.js
@@ -243,16 +243,30 @@ describe('<RegisterChallengePaymentMessages>', () => {
       cy.fixture('apiGetMyOrganizationAdmin.json').then((data) => {
         cy.dataCy('registration-coordinator-details')
           .should('be.visible')
-          .and(
-            'contain',
-            i18n.global.t(
-              'register.challenge.textRegistrationWaitingForCoordinatorWithNameAndEmail',
-              {
-                name: data.organization_admin[0].admin_name,
-                email: data.organization_admin[0].admin_email,
-              },
-            ),
-          );
+          .then(($el) => {
+            // element contains text
+            const textContent = $el.text();
+            cy.stripHtmlTags(
+              i18n.global.t(
+                'register.challenge.textRegistrationWaitingForCoordinatorWithNameAndEmail',
+                {
+                  name: data.organization_admin[0].admin_name,
+                  email: data.organization_admin[0].admin_email,
+                },
+              ),
+            ).then((text) => {
+              expect(textContent).to.contain(text);
+            });
+            // element has a mailto href attribute with email
+            cy.wrap($el)
+              .find('a')
+              .should('have.attr', 'href')
+              .should(
+                'include',
+                `mailto:${data.organization_admin[0].admin_email}`,
+              );
+          });
+
         cy.dataCy('registration-coordinator-details-not-available').should(
           'not.exist',
         );
