03c09a952eacca814f7338ab54c319b33baff101
Šimon Macek <simon.macek@proficio.cz>
2025-04-09 16:16:05 +0200

    remove disable for input

--- src/components/routes/RouteInputDistance.vue
+++ src/components/routes/RouteInputDistance.vue
@@ -146,7 +146,6 @@ export default defineComponent({
           <q-input
             dense
             outlined
-            :disable="action === RouteInputType.copyYesterday"
             reverse-fill-mask
             v-model="distance"
             :mask="
