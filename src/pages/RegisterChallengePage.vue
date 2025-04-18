8af1d2cff17650705c49895ea8b29bd51e1e247e
Šimon Macek <simon.macek@proficio.cz>
2025-04-18 23:19:53 +0200

    add condition for sending checkIsUserOrganizationAdmin

--- src/pages/RegisterChallengePage.vue
+++ src/pages/RegisterChallengePage.vue
@@ -147,9 +147,9 @@ export default defineComponent({
 
     onMounted(async () => {
       // check if user is organization admin
-      // if (registerChallengeStore.getIsUserOrganizationAdmin === null) {
-      //   await registerChallengeStore.checkIsUserOrganizationAdmin();
-      // }
+      if (registerChallengeStore.getIsUserOrganizationAdmin === null) {
+        await registerChallengeStore.checkIsUserOrganizationAdmin();
+      }
       // make sure price level is loaded
       if (!challengeStore.getPriceLevel.length) {
         await challengeStore.loadPhaseSet();
