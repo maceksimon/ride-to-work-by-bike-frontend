2c5d795a47296e7319fa0c85b68b867cdbda42e1
Šimon Macek <simon.macek@proficio.cz>
2025-08-11 22:58:19 +0200

    add exception for CY test into the component

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -2760,7 +2760,6 @@ Cypress.Commands.add(
           statusCode: responseStatusCode
             ? responseStatusCode
             : httpSuccessfullStatus,
-          delay: interceptedRequestResponseDelay,
           body: responseBody ? responseBody : defaultResponseBody,
         }).as('postResetPasswordConfirm');
       },
