f0f4fe303d05493825d87315da47a916de55a5df
Šimon Macek <simon.macek@proficio.cz>
2025-08-20 23:43:56 +0200

    fix cypress tests

--- test/cypress/support/commands.ts
+++ test/cypress/support/commands.ts
@@ -1,5 +1,5 @@
 // ***********************************************
-// This example commands.js shows you how to
+// This example commands.ts shows you how to
 // create various custom commands and overwrite
 // existing commands.
 //
@@ -21,7 +21,7 @@
 // Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
 //
 //
-// -- This is will overwrite an existing command --
+// -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
 // DO NOT REMOVE
